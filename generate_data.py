bbd74b287390a025e91ff70833cb030ecb4899a8
wrath <jovanprodanov123@gmail.com>
2023-05-23 17:43:05 -0700

    wip: add train loop and generation of data

---
+++ generate_data.py
@@ -0,0 +1,18 @@
+import numpy as np
+
+def generate_training_data(file_path, input_size, output_size, num_samples):
+    inputs = np.random.rand(num_samples, input_size)
+    labels = np.random.rand(num_samples, output_size)
+
+    with open(file_path, 'w') as file:
+        for i in range(num_samples):
+            input_values = ','.join(str(value) for value in inputs[i])
+            label_values = ','.join(str(value) for value in labels[i])
+            file.write(f"{input_values},{label_values}\n")
+
+file_path = "random_data.txt"
+input_size = 4
+output_size = 3
+num_samples = 150
+
+generate_training_data(file_path, input_size, output_size, num_samples)
--- src/main.c
+++ src/main.c
@@ -3,24 +3,62 @@
 
 #include "mlp.h"
 
+void load_training_data(const char* file_path, double** inputs, double** labels, const int num_samples,
+                        const int input_size, const int output_size)
+{
+	FILE* file = fopen(file_path, "r");
+	if (file == NULL)
+	{
+		printf("Error opening file: %s\n", file_path);
+		return;
+	}
+
+	for (int i = 0; i < num_samples; i++)
+	{
+		for (int j = 0; j < input_size; j++)
+		{
+			fscanf(file, "%lf,", &inputs[i][j]);
+		}
+		for (int j = 0; j < output_size; j++)
+		{
+			fscanf(file, "%lf,", &labels[i][j]);
+		}
+	}
+
+	fclose(file);
+}
+
 int main(void)
 {
-	mlp* mlp = create_mlp(4, 10, 3);
+	const char* file_path = "data/random_data.txt";
+	const int input_size = 4;
+	const int output_size = 3;
+	const int hidden_size = 10;
+	const int num_samples = 150;
+	const double learning_rate = 0.1;
+	const int epochs = 2;
 
-	const double input[4] = {0.5, 0.3, -0.2, 0.7};
-
-	double* output = forward(mlp, input);
-
-	printf("output: ");
-	for (int i = 0; i < mlp->output_size; i++)
+	double** inputs = malloc(num_samples * sizeof(double*));
+	double** labels = malloc(num_samples * sizeof(double*));
+	for (int i = 0; i < num_samples; i++)
 	{
-		printf("%.4f ", output[i]);
+		inputs[i] = malloc(input_size * sizeof(double));
+		labels[i] = malloc(output_size * sizeof(double));
 	}
-	printf("\n");
 
-	// free
-	free(output);
-	free_mlp(mlp);
+	load_training_data(file_path, inputs, labels, num_samples, input_size, output_size);
+
+	mlp* network = create_mlp(input_size, hidden_size, output_size);
+	train(network, inputs, labels, num_samples, learning_rate, epochs);
+
+	for (int i = 0; i < num_samples; i++)
+	{
+		free(inputs[i]);
+		free(labels[i]);
+	}
+	free(inputs);
+	free(labels);
+	free_network(network);
 
 	return 0;
 }
