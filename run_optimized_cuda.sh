aacca12bf7526d48f13e44ad406e31b2a7add07f
wrathchild14 <jovanprodanov123@gmail.com>
2023-05-30 23:21:22 +0200

    update run scripts

--- run_optimized_cuda.sh
+++ run_optimized_cuda.sh
@@ -2,7 +2,7 @@
 
 module load CUDA/10.1.243-GCC-8.3.0
 
-nvcc -O3 -o mlp_out src/cuda/main.cu -lcudart -lm
+nvcc -O3 -o mlp_out src/cuda_optimizations/main.cu -lcudart -lm
 
 # Run the executable
 srun --reservation=fri -G1 -n1 mlp_out
